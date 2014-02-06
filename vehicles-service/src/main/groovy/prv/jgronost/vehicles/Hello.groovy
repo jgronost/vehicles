df23755ffd987840074813426dbbe0bc51b1d62c
jgronost <jgronost@gmail.com>
2014-02-06 23:06:52 +0100

    hello from groovy

---
+++ vehicles-service/src/main/groovy/prv/jgronost/vehicles/Hello.groovy
@@ -0,0 +1,14 @@
+package prv.jgronost.vehicles
+
+class Hello {
+    def sayHello(name) {
+        println("Hello $name!")
+    }
+    
+    static def main(args) {
+        Hello hello = new Hello()
+        def name = 'world'
+        hello.sayHello(name)
+    }
+
+}
